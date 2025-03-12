da096acea475f2a4e3a1ce1a376b8e51fb448d05
Afad Fath Musyarof Halim <afadfath03@gmail.com>
2025-03-12 23:26:15 +0700

    menambahkan class DataGeneric

--- Program.cs
+++ Program.cs
@@ -3,11 +3,27 @@
         Console.Write("Masukkan nama anda: ");
         string nama = Console.ReadLine();
         HaloGeneric.SapaUser(nama);
+
+        Console.Write("\nMasukkan data dengan NIM anda : ");
+        string nim = Console.ReadLine();
+        DataGeneric<string> data = new DataGeneric<string>(nim);
+        data.PrintData();
     }
 }
 
 class HaloGeneric {
     public static void SapaUser(string namauser) {
         Console.WriteLine("Halo user " + namauser);
+    }
+}
+
+class DataGeneric<T> {
+    T data;
+    public DataGeneric(T data) {
+        this.data = data;
+    }
+
+    public void PrintData() {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
     }
 }
